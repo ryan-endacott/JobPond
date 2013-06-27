4e5a465fb19d920ffd8dc887a7b4578af8e047a5
ShaneBurkhart <shaneburkhart@gmail.com>
2013-06-27 18:25:47 -0500

    Init Employees controller

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,19 @@
+class EmployeesController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 JobApp::Application.routes.draw do
-  get "listings/index"
-
-  get "listings/show"
-
   authenticated :user do
     root :to => 'home#index'
   end
