c9e35e80db180f41c20aebb9d22ad40724a5e7db
ShaneBurkhart <shaneburkhart@gmail.com>
2013-06-26 22:03:02 -0500

    Modified role test without Rolify

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,15 +1,28 @@
+# == Schema Information
+#
+# Table name: roles
+#
+#  id            :integer          not null, primary key
+#  name          :string(255)
+#  resource_id   :integer
+#  resource_type :string(255)
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 require 'spec_helper'
 
 describe Role do
 
 	before do
-		@roles = [:employee, :employer]
+		@roles = [:employee?, :employer?, :admin?]
+		@user = FactoryGirl.create
 	end
 
 	it "should have appropriate roles" do 
 		@roles.each do |role|
-			r = Role.find_by_name(role)
-			r.should_not be_nil
+			user = User.all.first
+			user.should respond_to role
 		end
 	end
 
