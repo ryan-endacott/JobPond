f6b77cb6616b282ed5ea6c682a03f81bbc2b1041
ShaneBurkhart <shaneburkhart@gmail.com>
2013-06-27 18:17:55 -0500

    Basic routes. Will probably change

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,17 @@ JobApp::Application.routes.draw do
   root :to => "home#index"
   devise_for :users
   resources :users
+  #left the above for default although users probably needs to be singular
+  #users might not be needed at all since we have subclasses
+
+  resource :employee do
+  	resource :resume
+  end
+  resources :employer do
+  	resources :jobs
+  end
+
+  #cant use the jobs controller.  Need different layout and features.
+  resources :jobs, controller: :listings
+
 end
