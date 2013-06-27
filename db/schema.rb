3179ea255bdf8cb3b7826a8d8e8065f1cfeff540
Ryan Endacott <rzeg24@gmail.com>
2013-06-26 22:04:45 -0500

    added type to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130627014847) do
+ActiveRecord::Schema.define(:version => 20130627030259) do
 
   create_table "roles", :force => true do |t|
     t.string   "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(:version => 20130627014847) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.string   "type"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
