fe6c68b7bb6ec2331f99163b9d3439351144914c
Ryan Endacott <rzeg24@gmail.com>
2013-06-30 21:26:06 -0500

    Added the start of employer dashboard route.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,11 +12,11 @@ module ApplicationHelper
     html.html_safe
   end
 
-  def nav_link(link_text, link_path)
+  def nav_link(link_text, link_path, options = nil)
     class_name = current_page?(link_path) ? 'active' : ''
 
     content_tag(:li, :class => class_name) do
-      link_to link_text, link_path
+      link_to link_text, link_path, options
     end
   end
 
