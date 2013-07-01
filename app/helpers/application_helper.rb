be75cb0030ae0f60598b7ca0095afccbabedcd4a
Ryan Endacott <rzeg24@gmail.com>
2013-06-30 19:57:21 -0500

    nav link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,12 @@ module ApplicationHelper
     html.html_safe
   end
 
+  def nav_link(link_text, link_path)
+    class_name = current_page?(link_path) ? 'active' : ''
+
+    content_tag(:li, :class => class_name) do
+      link_to link_text, link_path
+    end
+  end
+
 end
