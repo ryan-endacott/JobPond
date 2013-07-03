cb395827e63e80ad09e1ea27dcb0e67cf119eb8e
ShaneBurkhart <shaneburkhart@gmail.com>
2013-07-03 00:11:48 -0500

    Changed devise scope to singular

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ JobApp::Application.routes.draw do
     root :to => 'marketing#employees'
   end
   root :to => 'marketing#employees'
-  #devise_for :users#, :controllers => {:registrations => "registrations"}
-  devise_scope :users do
+  devise_for :users#, :controllers => {:registrations => "registrations"}
+  devise_scope :user do
     get "employee/new", to: "devise/registrations#new"
     get "employer/new", to: "devise/registrations#new"
+    root to: "marketing#employees"
   end
   #resources :users
   #left the above for default although users probably needs to be singular
