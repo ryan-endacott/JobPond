d9809aecc0dff12cbd2db00e120411b9d3b944d1
Ryan Endacott <rzeg24@gmail.com>
2013-07-08 22:48:36 -0500

    before filter

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
+  skip_before_filter :authenticate_scope!, :only => [:edit, :update, :destroy]
+
   def sign_up(resource_name, resource)
     super(:user, resource)
   end
