1309bbe8236b9a90406df4f3257eda1ec56ee37d
ShaneBurkhart <shaneburkhart@gmail.com>
2013-07-10 17:08:08 -0500

    Added some more stuff to resume

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -11,6 +11,9 @@
 #
 
 class Resume < ActiveRecord::Base
-  attr_accessible :address, :job_experience, :phone_number
+  attr_accessible :address, :city, :state, :zipcode,
+   	:phone_number,
+   	:job_experience
+
   belongs_to :employee
 end
