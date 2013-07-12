f14cfc65b776f747e610769c023a275c06354844
Ryan Endacott <rzeg24@gmail.com>
2013-07-11 22:17:45 -0500

    annotate

--- db/schema.rb
+++ db/schema.rb
@@ -70,7 +70,8 @@ ActiveRecord::Schema.define(:version => 20130712014623) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "name"
+    t.string   "first_name"
+    t.string   "last_name"
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
