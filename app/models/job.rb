f2e20b298c66b40858bc19c9bb2de67d431472c2
Daniel <dansdebate@gmail.com>
2013-07-16 22:51:00 -0500

    migrated database and removed employers link

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,7 +4,7 @@
 #
 #  id          :integer          not null, primary key
 #  title       :string(255)
-#  description :text
+#  description :string(255)
 #  pay         :decimal(, )
 #  employer_id :integer
 #  created_at  :datetime         not null
