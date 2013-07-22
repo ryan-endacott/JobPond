197f4946bb5879ccd997583aeca7ce1cf972b257
Daniel <dansdebate@gmail.com>
2013-07-21 23:12:03 -0500

    added validations for jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -16,4 +16,12 @@ class Job < ActiveRecord::Base
 	has_many :applieds
   has_many :applicants, :through => :applieds, :source => :employee
   attr_accessible :description, :pay, :title, :employer_id
+
+validates :title, length: { in: 1..50,
+  	too_short: "Title must be longer"
+  	too_long: "Title can only be %{count} characters long" }
+  validates :description, length: { maximum: 500,
+  	too_long: "Description can only be %{count} characters long"}
+  validates :pay, numericality: true, greater_than_or_equal_to: 0
 end
+
