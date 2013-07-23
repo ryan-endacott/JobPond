8a716add961a8fecc3689aa9d2c21e0fbbb3b7c5
ShaneBurkhart <shaneburkhart@gmail.com>
2013-07-23 00:13:02 -0500

    Bootstrap theme.  also basic resizing for landing page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,40 @@
 //= require bootstrap
 //= require_tree .
 //= require cocoon
+
+var CHILD_MARGIN_TOP = 100,
+    FLOAT_OF_SCREEN_HEIGHT = .85;
+
+var map_resize = function(){
+  console.log("Resizing map...");
+  var map_container = $("#map-heading-wrapper"),
+    map_canvas = $("#map-canvas"),
+    map_promo_text = map_container.find("#map-heading-promo-text"),
+    map_sign_up = map_container.find("#map-heading-sign-up");
+
+  var window_width = $(window).width(),
+    window_height = $(window).height(),
+    container_width = $(".container").width();
+  //Do the map container so content will fit right in under
+  map_container.height(window_height * FLOAT_OF_SCREEN_HEIGHT);
+  //Do the actual map.  Probably not necessary with css but might as well.
+  map_canvas.height(window_height * FLOAT_OF_SCREEN_HEIGHT);
+  //Do the promo text
+  map_promo_text.css({
+    left : (window_width - container_width) / 2,
+    top : CHILD_MARGIN_TOP
+  });
+  //Do the sign up dialog
+  map_sign_up.css({
+    right : (window_width - container_width) / 2,
+    top : CHILD_MARGIN_TOP
+  });
+};
+
+$(document).ready(function(){
+  map_resize();
+  $(window).resize(function(){
+    map_resize();
+  });
+
+});
--- app/controllers/marketing_controller.rb
+++ app/controllers/marketing_controller.rb
@@ -4,6 +4,8 @@ class MarketingController < ApplicationController
 		redirect_to listings_path if user_signed_in?
 
 		@employee = Employee.new
+
+    @listing = Job.first
 	end
 
 end
