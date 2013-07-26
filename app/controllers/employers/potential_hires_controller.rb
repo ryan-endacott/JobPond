adba38ba88e434cca183923b62fcf26fea2b3e83
Ryan Endacott <rzeg24@gmail.com>
2013-07-25 21:40:53 -0500

    fix job seekers union

--- app/controllers/employers/potential_hires_controller.rb
+++ app/controllers/employers/potential_hires_controller.rb
@@ -2,6 +2,6 @@ class Employers::PotentialHiresController < ApplicationController
   before_filter :employer_only!
 
   def index
-    @job_seekers = current_user.jobs.map(&:applicants) | Employee.where(can_contact: true)
+    @job_seekers = current_user.jobs.map(&:applicants).flatten | Employee.where(can_contact: true)
   end
 end
