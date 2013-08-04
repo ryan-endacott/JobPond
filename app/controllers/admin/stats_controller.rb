24444d431ab1134fa883037a2df05a9186a36501
Daniel <dansdebate@gmail.com>
2013-08-03 20:03:22 -0500

    Added stats page

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -1,2 +1,10 @@
 class Admin::StatsController < ApplicationController
+	def index
+		@user_count = User.count
+		@employee_count = Employee.count
+		@employer_count = Employer.count
+		@admin_count = Admin.count
+		@job_count = Job.count
+		@contact_purchase_count = ContactPurchase.count
+	end
 end
